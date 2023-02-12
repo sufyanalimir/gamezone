ad412f72d5e073fd3bd1188173af7806cc152957
sufyanali <sufyanalimir313@gmail.com>
2023-02-12 17:24:15 +0530

    Added styles to home dot php

--- Home.php
+++ Home.php
@@ -1,20 +1,35 @@
 <?php
-    $uid = $_POST['uid'];
+    // $uid = $_POST['uid'];
+    $uid = 2;
 
 echo'
 <html>
     <head>
-        
+    <link rel="stylesheet" href="./assets/css/home.css">
     </head>
     <body>
-        <form action="userDetails.php" method="post">
-            <label>Uid</label>&nbsp; <input type="text" name="uid" id="uid" value='.$uid.' readonly/>
-            <input type="submit" name="viewScore" value="View Score" />
-        </form><button id="logout" onclick="logout()">LogOut</button><br><br>
-        <a href="FastTap.html?id='.$uid.'" target="gameFrame">FastTap</a>&nbsp;
-        <a href="FastMath.html?id='.$uid.'" target="gameFrame">FastMath</a>&nbsp;
-        <a href="TicTacToe.html?id='.$uid.'" target="gameFrame">TicTacToe</a>&nbsp;<br><br>
-        <iframe name="gameFrame" srcdoc="Select A game" frameborder="0" height="100%" width="100%"></iframe>
+        <div class="header-group">
+            <form action="userDetails.php" method="post">
+                <div>
+                    <label>UID</label>&nbsp; <input type="text" name="uid" id="uid" value='.$uid.' readonly/>
+                </div>
+                <input type="submit" name="viewScore" value="View Score" />
+            </form>
+            <div class="logout-btn">
+                <button id="logout" onclick="logout()">Log Out</button><br><br>
+            </div>
+        </div>
+        <div class="game-links">
+            <div>
+                <a href="FastTap.html?id='.$uid.'" target="gameFrame">Fast Tap</a>&nbsp;
+                <a href="FastMath.html?id='.$uid.'" target="gameFrame">Fast Math</a>&nbsp;
+                <a href="TicTacToe.html?id='.$uid.'" target="gameFrame">Tic Tac Toe</a>&nbsp;<br><br>
+            </div>
+            <div>
+                <p>SELECT A GAME</p>
+            </div>
+        </div>
+        <iframe name="gameFrame" frameborder="0" height="100%" width="100%"></iframe>
 
         <script type="text/javascript">
             function logout(){
